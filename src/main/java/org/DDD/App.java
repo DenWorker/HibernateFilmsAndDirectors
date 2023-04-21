6bf209a0cb68b8c944662daf85c8459735bd6d2e
DenWorker <whiteskyD@yandex.ru>
2023-04-21 15:31:10 +0300

    Add DB processing methods

--- src/main/java/org/DDD/App.java
+++ src/main/java/org/DDD/App.java
@@ -6,28 +6,77 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class App {
+    public static Configuration configuration = new Configuration().addAnnotatedClass(Director.class).addAnnotatedClass(Film.class);
+    public static SessionFactory sessionFactory = configuration.buildSessionFactory();
+    public static Session session = sessionFactory.getCurrentSession();
+
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Director.class).addAnnotatedClass(Film.class);
-
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
-
         try {
             session.beginTransaction();
 
-            Film film = session.get(Film.class, 2);
-            System.out.println(film);
 
-            Director director = session.get(Director.class, 1);
-            System.out.println(director);
+            // Вставляем методы. Не забываем про то, как работают сессии.
+            // После каждого изменения необходимо session.getTransaction().commit().
 
-            director.getListOfFilms().forEach(System.out::println);
 
-            session.getTransaction();
+            session.getTransaction().commit();
         } finally {
             sessionFactory.close();
         }
+    }
+
+    public static void printDirectorsInformation(int id) {
+        System.out.println("Информация о режисёре с id: " + id);
+        System.out.println(session.get(Director.class, id).toString());
+        System.out.println("Информация о фильмах этого режисёра:");
+        session.get(Director.class, id).getListOfFilms().forEach(t -> System.out.println(t.toString()));
+        System.out.println("-----------------------------------------");
+    }
+
+    public static void printFilmsInformation(int id) {
+        System.out.println("Информация о фильме с id: " + id);
+        System.out.println(session.get(Film.class, id).toString());
+        System.out.println("Информация о режисёре этого фильма:");
+        System.out.println(session.get(Film.class, id).getDirector().toString());
+        System.out.println("-----------------------------------------");
+    }
+
+    public static void addNewFilm(String nameOfFilm, int yearOfProductionsFilm, int idOfDirector) {
+        Director director = session.get(Director.class, idOfDirector);
+        Film film = new Film(nameOfFilm, yearOfProductionsFilm, director);
+
+        director.getListOfFilms().add(film);
+
+        session.save(film);
+        session.save(director);
+    }
+
+    public static void addNewDirector(String nameOfDirector, int ageOfDirector) {
+        Director director = new Director(nameOfDirector, ageOfDirector);
+
+
+        director.setListOfFilms(new ArrayList<>(Collections.singletonList(null)));
+        session.save(director);
+    }
+
+    public static void editDirectorOfFilm(int idOfFilm, int idOfNewDirector) {
+        Film film = session.get(Film.class, idOfFilm);
+        Director director = session.get(Director.class, idOfNewDirector);
+
+        film.getDirector().getListOfFilms().remove(film);
+        director.getListOfFilms().add(film);
+        film.setDirector(director);
+    }
+
+    public static void deleteFilmOfDirector(int idOfFilm) {
+        Film film = session.get(Film.class, idOfFilm);
+
+        film.getDirector().getListOfFilms().remove(film);
+        session.remove(film);
     }
 }
